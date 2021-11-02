ef5bde78d40cef4ca1ddf50aa025ff7a8c7a4847
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2021-11-02 18:50:51 -0400

    Create selection_sort.java

---
+++ sorting/selection_sort.java
@@ -0,0 +1,25 @@
+\**
+Selection sort works by sorting an array in ascending order. It iterates through an entire array. 
+For the ith iteration, the ith place is swapped with the smallest number, excluding numbers before i. (which are already sorted) 
+**\
+import java.util.*;
+public class test{
+  public static int[] selectionSort(int[] arr){
+    int N = arr.length;
+    for(int i=0;i<N-1;i++){
+        int minIndex = i;
+        for(int j=i+1;j<N;j++){
+            if(arr[j] < arr[minIndex]) minIndex = j;
+        }
+        int min = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = min;
+    }
+    return arr;
+
+  }
+  public static void main(String[] args){
+       int arr[] = {64,25,12,22,11};
+       System.out.println("Sorted array: " + Arrays.toString(selectionSort(arr)));
+  }
+}
